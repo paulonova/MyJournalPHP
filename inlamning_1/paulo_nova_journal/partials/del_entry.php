e717c6dc168e0109599ea367131e74dd73d90c75
paulonova <paulonova144@gmail.com>
2018-04-24 20:43:02 +0200

    Delete entries

--- inlamning_1/paulo_nova_journal/partials/del_entry.php
+++ inlamning_1/paulo_nova_journal/partials/del_entry.php
@@ -1,6 +1,10 @@
 <?php 
 // header('Location: /journal.php');
 require_once 'session_start.php';
+require_once "classes/entry.php";
+require_once 'partials/database.php';
+
+header('Location: /journal.php');
 
 echo "Delete Test: " . $_SESSION['user_id'];
 
